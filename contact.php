8f1081234b9b0f66a238171a2b08eb421d85c7de
Sarita Rimal <be2017se593@gces.edu.np>
2023-05-19 17:51:14 +0545

    Your commit message here

---
+++ contact.php
@@ -0,0 +1,256 @@
+<?php
+include('header1.php')
+  ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>About us</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicon -->
+  <link href="img/favicon.ico" rel="icon">
+
+  <!-- Google Web Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&display=swap"
+    rel="stylesheet">
+
+  <!-- Icon Font Stylesheet -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+  <!-- Libraries Stylesheet -->
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+  <!-- Customized Bootstrap Stylesheet -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="./bootstrap.min.css" rel="stylesheet">
+  <!-- Template Stylesheet -->
+  <link href="./css/style1.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+  <link href="./css/contact.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+    integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+
+<body>
+  <!-- Spinner Start 
+    <div id="spinner"
+        class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+     Spinner End -->
+
+
+  <!-- Navbar Start 
+    <nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
+        <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+            <div class="logo">
+                <img src="../img/logo.png" alt="">
+            </div>
+        </a>
+        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto p-4 p-lg-0">
+                <a href="index.html" class="nav-item nav-link">Home</a>
+                <a href="about.html" class="nav-item nav-link active">About</a>
+                <a href="courses.html" class="nav-item nav-link">Courses</a>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                    <div class="dropdown-menu fade-down m-0">
+                        <a href="team.html" class="dropdown-item">Our Team</a>
+                        <a href="testimonial.html" class="dropdown-item">Testimonial</a>
+                        <a href="404.html" class="dropdown-item">404 Page</a>
+                    </div>
+                </div>
+                <a href="contact.html" class="nav-item nav-link">Contact</a>
+            </div>
+            <a href="" class="btn btn-primary py-4 px-lg-5 d-none d-lg-block">Join Now<i
+                    class="fa fa-arrow-right ms-3"></i></a>
+        </div>
+    </nav>
+
+
+
+    Header Start -->
+  <div class="container-fluid bg-primary py-5 mb-5 page-header">
+    <div class="container py-5">
+      <div class="row justify-content-center">
+        <div class="col-lg-10 text-center">
+          <h1 class="display-3 text-white animated slideInDown">Contact us</h1>
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb justify-content-center">
+              <li class="breadcrumb-item"><a class="text-white" href="#">Home</a></li>
+
+              <li class="breadcrumb-item text-white active" aria-current="page">contact</li>
+            </ol>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Header End -->
+
+
+  <!-- Service Start -->
+  <h1 class="title">Ready To Learn More?​</h1>
+  <p class="sub">Call us on 1300 424 442 to talk about your needs Or fill out the form below and our Care Advisors
+    will be in touch
+    soon.</p>
+  <style>
+    @media (max-width: 1024px) {
+      .title {
+        font-size: 22px;
+      }
+    }
+
+    .sub {
+      color: #3A5668;
+      font-family: "Vollkorn", Sans-serif;
+      font-size: 22px;
+      font-weight: 400;
+      line-height: 1.6em;
+      text-align: center;
+    }
+
+    .title {
+      color: #3A5668;
+      font-family: "Vollkorn", Sans-serif;
+      font-size: 36px;
+      font-weight: 400;
+      line-height: 1.6em;
+      text-align: center;
+    }
+  </style>
+  <div class="container-xxl py-5">
+    <div class="container">
+      <div class="row g-4">
+        <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
+          <div class="service-item text-center pt-3">
+            <div class="p-4">
+              <i class="fa-solid fa fa-3x fa-map-marker-alt text-primary mb-4" style="color:#274a84;"></i>
+              <h5 class="mb-3">Address</h5>
+              <p>3 Grumman Court, Zuccoli, Northern Territory 0832</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
+          <div class="service-item text-center pt-3">
+            <div class="p-4">
+              <i class="fa-solid fa fa-3x fa-mobile-alt text-primary mb-4" style="color:#274a84;"></i>
+              <h5 class="mb-3">Phone Number</h5>
+              <p>1300 424 442</p>
+              <br>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
+          <div class="service-item text-center pt-3">
+            <div class="p-4">
+
+              <i class="fa-solid fa fa-3x fa-envelope-open text-primary mb-4" style="color: #274a84;"></i>
+              <h5 class="mb-3">Mail</h5>
+              <p>Info@hopeability.com.au</p>
+              <br>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
+          <div class="service-item text-center pt-3">
+            <div class="p-4">
+              <i class="fa-solid  fa fa-3x fa-clock text-primary mb-4" style="color: #274a84;"></i>
+              <h5 class="mb-3">We Are Open</h5>
+              <p>Monday – Friday : 9AM – 5PM</p>
+              <br>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Service End -->
+
+  <!-- Back to Top -->
+  <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+
+  <!-- JavaScript Libraries -->
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/waypoints/waypoints.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+  <!-- Template Javascript -->
+  <script src="js/main.js"></script>
+
+  <div class="container-1">
+    <div class="apply_box">
+      <h1>
+        For Enquiry Form
+      </h1>
+      <br>
+      <form action="https://formspree.io/f/mlekoogj" method="POST">
+        <div class="form_container">
+          <div class="form_control">
+            <label for="first_name"> First Name </label>
+            <input id="first_name" name="first_name" placeholder="Enter First Name..." />
+          </div>
+          <div class="form_control">
+            <label for="last_name"> Last Name </label>
+            <input id="last_name" name="last_name" placeholder="Enter Last Name..." />
+          </div>
+          <div class="form_control">
+            <label for="email"> Email </label>
+            <input type="email" id="email" name="email" placeholder="Enter Email..." />
+          </div>
+          <div class="form_control">
+            <label for="job_role"> Phone </label>
+            <input id="phone" name="phone" placeholder="Enter Phone...." />
+          </div>
+          <div class="textarea_control">
+            <label for="address"> Message </label>
+            <textarea id="address" name="address" row="4" cols="50" placeholder="Enter Message"></textarea>
+          </div>
+
+          <div class="button_container">
+            <button type="submit">Submit</button>
+          </div>
+      </form>
+    </div>
+  </div>
+  <br>
+  <br>
+  </div>
+  <div class="map">
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d973.7902539402759!2d131.01108536954771!3d-12.505484499235527!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2cc0a311b353d393%3A0xda11542df27d0569!2s3%20Grumman%20Cres%2C%20Zuccoli%20NT%200832%2C%20Australia!5e0!3m2!1sen!2snp!4v1684496714930!5m2!1sen!2snp"
+      width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+  </div>
+
+
+  </div>
+
+  </section>
+</body>
+<?php
+include('footer.php')
+  ?>
+
+</html>
---
+++ index.php
@@ -0,0 +1,216 @@
+<?php
+include('header1.php')
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Home</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&display=swap"
+        rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+
+    <!-- Carousel Start -->
+    <div class="container-fluid p-0 mb-5">
+        <div class="owl-carousel header-carousel position-relative">
+            <div class="owl-carousel-item position-relative">
+                <img class="img-fluid" src="./img/front-view-smiley-girl-woman-indoors.jpg" alt="">
+                <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center"
+                    style="background: rgba(24, 29, 56, .7);">
+                    <div class="container">
+                        <div class="row justify-content-start">
+                            <div class="col-sm-10 col-lg-8">
+
+                                <h1 class="display-1 text-white animated slideInDown">We Care With Respect</h1>
+                                <p class="fs-5 text-white mb-4 pb-2">Personalised Disability & Home Care Services.</p>
+                                <a href="" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">CLICK HERE
+                                    TO GET STARTED</a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="owl-carousel-item position-relative">
+                <img class="img-fluid"
+                    src="img/granddaughter-talking-with-her-grandmother-sitting-wheelchair-cheerful-concept-happy-family.jpg"
+                    alt="">
+                <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center"
+                    style="background: rgba(24, 29, 56, .7);">
+                    <div class="container">
+                        <div class="row justify-content-start">
+                            <div class="col-sm-10 col-lg-8">
+
+                                <h1 class="display-1 text-white animated slideInDown">We Care With Respect</h1>
+                                <p class="fs-5 text-white mb-4 pb-2">Personalised Disability & Home Care Services.</p>
+                                <a href="" class="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">CLICK HERE
+                                    TO GET STARTED</a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Carousel End -->
+
+
+
+
+    <!-- About Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="row g-5">
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s" style="min-height: 400px;">
+                    <div class="position-relative h-100">
+                        <img class="img-fluid position-absolute w-100 h-100" src="img/aboutus.jpg" alt=""
+                            style="object-fit: cover; border-radius: 15px 45px 15px 45px; ">
+                    </div>
+                </div>
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
+                    <h6 class="section-title bg-white text-start text-primary pe-3">About Us</h6>
+                    <h1 class="mb-4">We're All About Trust</h1>
+                    <p class="mb-4">Hope Ability management has years of experience in disability services along with
+                        extensive experience in customer focused public and private sector. Our vision is to empower
+                        participants to take ownership of their life.</p>
+                    <a class="btn btn-primary py-3 px-5 mt-2" href="">Read More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- About End -->
+
+
+    <!-- Services Start -->
+    <div class="container-xxl py-5 category">
+        <div class="container">
+            <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+                <h6 class="section-title bg-white text-center text-primary px-3">Services</h6>
+                <h1 class="mb-5">Our Services</h1>
+            </div>
+            <div class="row g-3">
+                <div class="col-lg-4 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="service-item text-center pt-3">
+                        <div class="p-4">
+                            <i class="fa fa-3x fa-trophy text-primary mb-4"></i>
+                            <h5 class="mb-3">Development-Life Skills</h5>
+                            <p>Development of daily living and life skills focues on training and development activites.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="service-item text-center pt-3">
+                        <div class="p-4">
+                            <i class="fa fa-3x fa-star text-primary mb-4"></i>
+                            <h5 class="mb-3">Personal Activites</h5>
+                            <p>Assisting with and/or supervising personal tasks of daily life to enable the participant
+                                to.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="service-item text-center pt-3">
+                        <div class="p-4">
+                            <i class="fa fa-3x fa-list-check text-primary mb-4"></i>
+                            <h5 class="mb-3">Assistance Life Tasks</h5>
+                            <p>This registration group incorporates assistance with or supervising tasks of daily life.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div style="display: flex; justify-content: center;"> <a class="btn btn-primary py-3 px-5 mt-4 "
+                    href="">Read More</a></div>
+        </div>
+
+    </div>
+    <!-- Services End -->
+
+
+    <!-- Courses Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="text-center wow fadeInUp" data-wow-delay="0.3s">
+                <h6 class="section-title bg-white text-center text-primary px-3">Mission</h6>
+                <h1 class="mb-5">Our Mission</h1>
+            </div>
+            <div class="row g-5">
+                <br><br><br>
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
+
+                    <p class="mb-3">Our mission is to empower people with a disability to engage in opportunities of
+                        their choice in a supported and inclusive environment.</p>
+
+                </div>
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s" style="min-height: 400px;">
+                    <div class="position-relative h-100">
+                        <img class="img-fluid position-absolute w-80 h-80" src="img/full-shot-woman-with-crutches.jpg"
+                            alt="" style="object-fit: cover; border-radius: 15px 45px 15px 45px; ">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Courses End -->
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+<?php
+include('footer.php')
+    ?>
+
+</html>
